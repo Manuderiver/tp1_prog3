ce30cd06d042726b20cdd0991e117378c8474d8d
sugga222 <aguslomas28q@gmail.com>
2026-05-11 18:05:14 -0300

    renderizado de servicios.js

--- js/servicios.js
+++ js/servicios.js
@@ -1,1 +1,28 @@
-fetch()
+const contenedor = document.getElementById("contenedor-servicios");
+
+const obtenerServicios = async () => {
+
+    try {
+
+        const respuesta = await fetch("http://localhost:3000/servicios");
+
+        const servicios = await respuesta.json();
+
+        servicios.forEach(servicio => {
+
+            contenedor.innerHTML += `
+                <div class="card-servicio">
+                    <h3>${servicio.nombre}</h3>
+                    <p>${servicio.descripcion}</p>
+                </div>
+            `;
+        });
+
+    } catch (error) {
+
+        console.log("Error al cargar servicios:", error);
+
+    }
+};
+
+obtenerServicios();
